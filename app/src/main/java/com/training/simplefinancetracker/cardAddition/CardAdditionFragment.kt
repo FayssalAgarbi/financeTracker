9d8770d37485952b223eccb67f36689348bbff63
faaga001 <fayssal.agarbi001@stud.fh-dortmund.de>
2021-12-08 00:55:23 +0100

    FEAT: Added a BottomSheetDialogFragment for adding expenditure-entries. It can be navigated to, but it's still dumb.

--- app/src/main/java/com/training/simplefinancetracker/cardAddition/CardAdditionFragment.kt
+++ app/src/main/java/com/training/simplefinancetracker/cardAddition/CardAdditionFragment.kt
@@ -1,5 +1,37 @@
 package com.training.simplefinancetracker.cardAddition
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.airbnb.mvrx.MavericksView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.training.simplefinancetracker.databinding.FragmentCardAdditionBinding
 
-class CardAdditionFragment {
+
+class CardAdditionFragment : BottomSheetDialogFragment(), MavericksView {
+
+   // private val binding: FragmentCardAdditionBinding by viewBinding()
+    private var _binding: FragmentCardAdditionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentCardAdditionBinding.inflate(inflater,  container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+
+
+    override fun invalidate() {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/training/simplefinancetracker/cardList/CardListFragment.kt
+++ app/src/main/java/com/training/simplefinancetracker/cardList/CardListFragment.kt
@@ -1,10 +1,9 @@
 package com.training.simplefinancetracker.cardList
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.airbnb.mvrx.MavericksView
 import com.airbnb.mvrx.fragmentViewModel
@@ -12,7 +11,6 @@ import com.airbnb.mvrx.withState
 import com.training.simplefinancetracker.R
 import com.training.simplefinancetracker.databinding.FragmentCardListBinding
 import com.training.simplefinancetracker.util.viewBinding
-import timber.log.Timber
 
 
 class CardListFragment : Fragment(R.layout.fragment_card_list), MavericksView {
@@ -23,12 +21,18 @@ class CardListFragment : Fragment(R.layout.fragment_card_list), MavericksView {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
+
         binding.navBackBt.setOnClickListener {
             findNavController().navigate(CardListFragmentDirections.actionCardListFragmentToMainMenuFragment())
         }
 
         binding.recycler.adapter = CardListAdapter()
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        findNavController().navigate(CardListFragmentDirections.actionCardListFragmentToCardAdditionFragment())
+        return true
     }
 
     override fun invalidate() = withState(viewModel){
